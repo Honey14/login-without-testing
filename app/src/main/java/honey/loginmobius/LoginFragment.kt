160e9587f9249e0fbee4263da600a82e96322e5b
Honey Sonwani <honeysonwani88@gmail.com>
2020-05-17 21:28:39 +0530

    Wire up with Mobius Loop

--- app/src/main/java/honey/loginmobius/LoginFragment.kt
+++ app/src/main/java/honey/loginmobius/LoginFragment.kt
@@ -5,14 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.spotify.mobius.Connection
+import com.spotify.mobius.Mobius
+import com.spotify.mobius.MobiusLoop
+import com.spotify.mobius.android.MobiusAndroid
+import com.spotify.mobius.functions.Consumer
 
 class LoginFragment : Fragment() {
 
-    override fun onCreateView(
+    private val effectHandler: LoginEffectHandler = TODO()
+
+    private val mobiusLoop: MobiusLoop.Builder<LoginModel, LoginEvent, LoginEffect> =
+        Mobius.loop(LoginUpdate(), effectHandler)
+
+    private val controller: MobiusLoop.Controller<LoginModel, LoginEvent> =
+        MobiusAndroid.controller(mobiusLoop, LoginModel.default())
+
+    override
+    fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return createView(inflater, container).also {
+            controller.connect(::connectEvents)
+        }
+    }
+
+    private fun createView(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): View {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    private fun connectEvents(events: Consumer<LoginEvent>): Connection<LoginModel> {
+
+        return object : Connection<LoginModel> {
+
+            override fun accept(model: LoginModel) {
+
+            }
+
+            override fun dispose() {
+
+            }
+        }
     }
 }
